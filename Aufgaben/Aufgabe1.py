d829ee56b42a2e98cb58e4806f06763ed569f147
Philipp <3002084@stud.hs-mannheim.de>
2025-09-29 21:45:43 +0200

    Aufgabe 1 first version

--- Aufgaben/Aufgabe1.py
+++ Aufgaben/Aufgabe1.py
@@ -1,13 +1,48 @@
 import numpy as np
-
-board = np.array([np.array(range(1, 10)) for _ in range(9)])
+import plotly.express as px
 
 
 def get_fitness(board: np.array) -> int:
     return -np.sum([len(set(board[:, i])) != 9 for i in range(9)])
 
 
-for row in board:
-    print(row)
+def swap(board: np.array) -> np.array:
+    row = np.random.randint(0, 9)
 
-print(get_fitness(board))
+    idx1 = np.random.randint(0, 9)
+    idx2 = np.random.randint(0, 9)
+    num1 = board[row, idx1]
+    num2 = board[row, idx2]
+
+    board[row, idx1] = num2
+    board[row, idx2] = num1
+
+    return board
+
+
+def main(board: np.array) -> None:
+    iter = 0
+    max_iter = 1000000
+    current_fitness = get_fitness(board)
+    while current_fitness < 0 and iter < max_iter:
+        new_board = board.copy()
+        new_board = swap(new_board)
+        new_fitness = get_fitness(new_board)
+
+        if new_fitness > current_fitness:
+            board = new_board
+            current_fitness = new_fitness
+            print(f"Iteration {iter}: neue Fitness: {current_fitness}")
+
+        iter += 1
+
+
+if __name__ == "__main__":
+    board = np.array([np.array(range(1, 10)) for _ in range(9)])
+
+    main(board)
+
+    for row in board:
+        print(row)
+
+    print(get_fitness(board))
---
+++ Aufgaben/Aufgabe2.py
@@ -0,0 +1,3 @@
+import numpy as np
+
+board = np.array([np.array(range(1, 10)) for _ in range(9)])
