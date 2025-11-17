6059315e585fbcb5f9ca108b281d8edc7829f63e
Philipp <3002084@stud.hs-mannheim.de>
2025-11-17 13:03:42 +0100

    KNN iterative solution

--- Aufgaben/Aufgabe5.py
+++ Aufgaben/Aufgabe5.py
@@ -1,4 +1,5 @@
 import numpy as np
+from collections import Counter
 
 
 def parse_mnist_data(
@@ -6,6 +7,7 @@ def parse_mnist_data(
     idx_file_training_labels: str,
     idx_file_test_samples: str,
     idx_file_test_labels: str,
+    num_samples: int,
 ) -> tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
     training_samples = parse_mnist_images(idx_file_training_samples)
     training_labels = parse_mnist_labels(idx_file_training_labels)
@@ -13,7 +15,12 @@ def parse_mnist_data(
     test_samples = parse_mnist_images(idx_file_test_samples)
     test_labels = parse_mnist_labels(idx_file_test_labels)
 
-    return training_samples, training_labels, test_samples, test_labels
+    return (
+        training_samples[:num_samples],
+        training_labels[:num_samples],
+        test_samples[:num_samples],
+        test_labels[:num_samples],
+    )
 
 
 def parse_mnist_images(idx_file_path: str) -> np.ndarray:
@@ -26,7 +33,7 @@ def parse_mnist_images(idx_file_path: str) -> np.ndarray:
         num_cols = int.from_bytes(f.read(4), "big")
 
         data = f.read()
-        out = np.ndarray((num_img, num_rows, num_cols), np.uint8, data)
+        out = np.ndarray((num_img, num_rows, num_cols), np.uint8, data) / 255.0
         return out
 
 
@@ -42,10 +49,41 @@ def parse_mnist_labels(idx_file_path: str) -> np.ndarray:
         return out
 
 
+def knn(test_sample, training_samples, training_labels, k):
+    distances = np.linalg.norm(training_samples - test_sample, axis=1)
+
+    k_lowest_idx = np.argsort(distances)[:k]
+
+    k_labels = training_labels[k_lowest_idx]
+    predicted = Counter(k_labels).most_common(1)[0][0]
+    return predicted
+
+
 if __name__ == "__main__":
+    k = 1
+    num_samples = 1000
     training_samples, training_labels, test_samples, test_labels = parse_mnist_data(
         "./assets/train_img.idx",
         "./assets/train_label.idx",
         "./assets/test_img.idx",
         "./assets/test_label.idx",
+        num_samples,
     )
+    training_samples_flat = training_samples.reshape(num_samples, -1)
+    test_samples_flat = test_samples.reshape(num_samples, -1)
+    training_labels = training_labels.flatten()
+    test_labels = test_labels.flatten()
+
+    correct = 0
+    total = len(test_samples)
+
+    for i in range(total):
+        if i % 100 == 0:
+            print(f"Case: {i} / {total}")
+        predicted = knn(test_samples_flat[i], training_samples_flat, training_labels, k)
+        true = test_labels[i]
+        if predicted == true:
+            correct += 1
+
+    accuracy = correct / total
+    print(f"Accuracy: {accuracy}")
